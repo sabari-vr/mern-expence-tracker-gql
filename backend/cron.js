dc0cbfc29f5b3d190fb1dbaa8aba0d185d3ec9b8
Sabari VR <sabari.r@nergyindia.com>
2024-10-16 11:38:30 +0530

    new cron url

--- backend/cron.js
+++ backend/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "https://mern-expence-tracker-gql.onrender.com/";
+const URL = "https://mern-expence-tracker-gql-lmdz.onrender.com";
 
 const job = new cron.CronJob("*/10 * * * *", function () {
   https
